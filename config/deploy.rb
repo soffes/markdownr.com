2b207c7ca2793957c76abe4a58e9fb2671a3e86e
Sam Soffes <sam@samsoff.es>
2009-08-30 01:10:13 -0500

    Updated cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,33 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "markdownr.com"
 
-# If you have previously been relying upon the code to start, stop 
-# and restart your mongrel application, or if you rely on the database
-# migration code, please uncomment the lines you require below
+role :app, "#{application}"
+role :web, "#{application}"
+role :db, "#{application}", :primary => true
 
-# If you are deploying a rails app you probably need these:
+set :deploy_to, "/var/www/#{application}"
 
-# load 'ext/rails-database-migrations.rb'
-# load 'ext/rails-shared-directories.rb'
+set :user, "samsoffes"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-# There are also new utility libaries shipped with the core these 
-# include the following, please see individual files for more
-# documentation, or run `cap -vT` with the following lines commented
-# out to see what they make available.
+# Git
+set :scm, "git"
+set :repository,  "git@github.com:samsoffes/#{application}.git"
+set :branch, "master"
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
 
-# load 'ext/spinner.rb'              # Designed for use with script/spin
-# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
-# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
-# see a full list by running "gem contents capistrano | grep 'scm/'"
-
-role :web, "your web-server here"
+# Tasks
+namespace :deploy do
+  
+  # Restart Passenger
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  # Nuke All
+  desc "Nuke entire deploy_to directory"
+  task :nuke_all do
+    run "rm -rf #{deploy_to}/*"
+  end
+end

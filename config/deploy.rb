6ed9df4e74fd2093d14a141dbfcf84278f4602e6
Sam Soffes <sam@samsoff.es>
2009-08-30 01:04:33 -0500

    Capify

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+set :application, "set your application name here"
+set :repository,  "set your repository location here"
+
+# If you have previously been relying upon the code to start, stop 
+# and restart your mongrel application, or if you rely on the database
+# migration code, please uncomment the lines you require below
+
+# If you are deploying a rails app you probably need these:
+
+# load 'ext/rails-database-migrations.rb'
+# load 'ext/rails-shared-directories.rb'
+
+# There are also new utility libaries shipped with the core these 
+# include the following, please see individual files for more
+# documentation, or run `cap -vT` with the following lines commented
+# out to see what they make available.
+
+# load 'ext/spinner.rb'              # Designed for use with script/spin
+# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
+# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+# see a full list by running "gem contents capistrano | grep 'scm/'"
+
+role :web, "your web-server here"

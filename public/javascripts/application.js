d14cbb7e0e0008d5e3f2076c11be11e652f31af1
samsoffes <sam@samsoff.es>
2009-09-12 17:13:22 -0600

    Added loading dots

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,13 +4,18 @@ $(function() {
   var notepad = $('textarea#notepad');
   var output = $('div#output');
   var cached_input = '';
+  var loading = $('div#loading')
   
   var process = function() {
     if (notepad.val() == cached_input) {
       return;
     }
+    
+    loading.show();
+    
     $.get(form.attr('action'), form.serialize(), function(data, textStatus) {
       output.html(data);
+      loading.fadeOut(100);
       cached_input = notepad.val();
     });
   };

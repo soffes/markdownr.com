d18147751a577a9b56d2c19c1a6ca600aed46b57
Sam Soffes <sam@samsoff.es>
2009-11-18 18:18:41 -0600

    Added Quantcast tag

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -22,4 +22,8 @@ module LayoutHelper
     # This can be overridden but passing true for the second argument
     (always || ENV['RAILS_ENV'] == 'production') ? "<script type=\"text/javascript\">var gaJsHost=((\"https:\"==document.location.protocol)?\"https://ssl.\":\"http://www.\");document.write(unescape(\"%3Cscript src='\"+gaJsHost+\"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));</script><script type=\"text/javascript\">try{var pageTracker=_gat._getTracker(\"#{id}\");pageTracker._trackPageview();}catch(err){}</script>" : ""
   end
+  
+  def quantcast(id, always = false)
+    (always || ENV['RAILS_ENV'] == 'production') ? "<script type=\"text/javascript\">_qoptions={qacct:\"#{id}\"};</script><script type=\"text/javascript\" src=\"http://edge.quantserve.com/quant.js\"></script><noscript><img src=\"http://pixel.quantserve.com/pixel/#{id}.gif\" style=\"display:none\" border=\"0\" height=\"1\" width=\"1\" alt=\"Quantcast\"/></noscript>" : ""
+  end
 end

2c2c95cafd06f53f5dafd740966f27846423c63b
Sam Soffes <sam@samsoff.es>
2010-01-14 22:43:49 -0600

    Hide/Show HTML

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,27 +1,50 @@
 $(function() {
   
+  var cached_input = '';
+  var show_html = false;
+  
   var form = $('form#notepad_form');
   var notepad = $('textarea#notepad');
-  var output = $('div#output');
-  var cached_input = '';
-  var loading = $('div#loading')
+  var markdown_output = $('div#markdown-output');
+  var html_output = $('textarea#html-output');
+  var toggle_output = $('a#toggle-output');
+  var loading = $('div#loading');
   
+  // Process markdown
   var process = function() {
     if (notepad.val() == cached_input) {
       return;
     }
-    
     loading.show();
-    
     $.get(form.attr('action'), form.serialize(), function(data, textStatus) {
-      output.html(data);
+      markdown_output.html(data);
+      html_output.val(data);
       loading.fadeOut(100);
       cached_input = notepad.val();
     });
   };
   
+  // Toggle output
+  var toggle = function(e) {
+    e.preventDefault();
+    show_html = !show_html;
+    
+    if (show_html) {
+      toggle_output.html('Hide HTML');
+      markdown_output.hide();
+      html_output.show();
+    } else {
+      toggle_output.html('Show HTML');
+      markdown_output.show();
+      html_output.hide();
+    }
+  };
+  
+  // Add event for keyup and focus
   notepad.keyup(process);
   notepad.focus();
   
-  process();
+  // Add event for toggle link
+  toggle_output.click(toggle);
+  
 });

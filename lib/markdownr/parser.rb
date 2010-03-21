5da9cfaae74fc2d18030cbd5e67f7accaacf8e00
Sam Soffes <sam@samsoff.es>
2010-03-21 01:20:05 -0500

    Improved stripping and now remove <script> instead of escape it

--- lib/markdownr/parser.rb
+++ lib/markdownr/parser.rb
@@ -6,24 +6,24 @@ module Markdownr
     def self.parse(text)
       return "" unless text && !text.empty?
       
+      text = preprocess(text)
+      
+      # Parse!
+      RDiscount.new(text).to_html
+    end
+    
+    def self.preprocess(text)
       # Extract pre blocks
       extractions = {}
       text.gsub!(%r{<pre>.*?</pre>}m) do |match|
         md5 = Digest::MD5.hexdigest(match)
         extractions[md5] = match
-        "{gfm-extraction-#{md5}}"
+        "{markdownr-extraction-#{md5}}"
       end
-
-      # prevent foo_bar_baz from ending up with an italic word in the middle
-      text.gsub!(/(^(?! {4}|\t)\w+_\w+_\w[\w_]*)/) do |x|
-        x.gsub('_', '\_') if x.split('').sort.to_s[0..1] == '__'
-      end
-
-      # in very clear cases, let newlines become <br /> tags
-      text.gsub!(/^[\w\<][^\n]*\n+/) do |x|
-        x =~ /\n{2}/ ? x : (x.strip!; x << "  \n")
-      end
-    
+      
+      # Remove <script>
+      text.gsub!(%r{<script(\s+(\w+)(?:\s*=\s*(?:".*?"|'.*?'|[^'">\s]+)))?>.*?</script>}m, '')
+      
       # Remove evil attributes. This could be a lot better
       evil_attributes = %w{onchange onsubmit onreset onselect onblur onfocus onkeydown onkeypress onkeyup onclick onmousedown onmousemove onmouseout onmouseover onmouseup}
       text.gsub!(%r{</?\w+(\s+(\w+)(?:\s*=\s*(?:".*?"|'.*?'|[^'">\s]+)))+\s*/?>}) do |match|
@@ -35,17 +35,23 @@ module Markdownr
         match.gsub!(/href(\s*=\s*(?:"javascript:[^"]*?"|'javascript:[^']*?'|javascript:[^'">\s]+))/, 'href="javascript:;"')
         match
       end
-          
-      # Remove <script>
-      text = CGI.escapeElement(text, "script")
+
+      # Prevent foo_bar_baz from ending up with an italic word in the middle
+      text.gsub!(/(^(?! {4}|\t)\w+_\w+_\w[\w_]*)/) do |match|
+        match.gsub('_', '\_') if match.split('').sort.to_s[0..1] == '__'
+      end
+
+      # In very clear cases, let newlines become <br /> tags
+      text.gsub!(/^[\w\<][^\n]*\n+/) do |match|
+        match =~ /\n{2}/ ? match : (match.strip!; match << "  \n")
+      end
       
       # Insert pre block extractions
-      text.gsub!(/\{gfm-extraction-([0-9a-f]{32})\}/) do
+      text.gsub!(/\{markdownr-extraction-([0-9a-f]{32})\}/) do
         "\n\n" + extractions[$1]
       end
-    
-      # Parse!
-      RDiscount.new(text).to_html
+      
+      text
     end
   end
 end
